90cb18bb81f19d7aff632256374178f036a70ebf
Subhasis mahata <subhasismahata@yahoo.com>
2023-02-05 14:17:49 +0530

    Update c.js

--- experiment/simulation/js/c.js
+++ experiment/simulation/js/c.js
@@ -11,102 +11,102 @@ function s3()
 	if(image5.src.match("on") && image6.src.match("on") && image3.src.match("on") && image4.src.match("on") && image2.src.match("on") && image1.src.match("off"))
 	{
 		console.log(7);
-		document.getElementById("i8").src="/images/seven.jpg";
+		document.getElementById("i8").src="images/seven.jpg";
 	}
 	else if(image5.src.match("on") && image6.src.match("on") && image3.src.match("on") && image4.src.match("on") && image1.src.match("off"))
 	{
 		console.log(3);
-		document.getElementById("i8").src="/images/three.jpg";
+		document.getElementById("i8").src="images/three.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image2.src.match("on") && image4.src.match("on") && image1.src.match("off"))
 	{
 		console.log(5);
-		document.getElementById("i8").src="/images/five.jpg";
+		document.getElementById("i8").src="images/five.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image2.src.match("on") && image3.src.match("on") && image1.src.match("off"))
 	{
 		console.log(6);
-		document.getElementById("i8").src="/images/six.jpg";
+		document.getElementById("i8").src="images/six.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image4.src.match("on") && image1.src.match("on") && image3.src.match("off") && image2.src.match("off"))
 	{
 		console.log(9);
-		document.getElementById("i8").src="/images/nine.jpg";
+		document.getElementById("i8").src="images/nine.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image3.src.match("on") && image1.src.match("off"))
 	{
 		console.log(2);
-		document.getElementById("i8").src="/images/two.jpg";
+		document.getElementById("i8").src="images/two.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image1.src.match("on") && image3.src.match("off") && image2.src.match("off"))
 	{
 		console.log(8);
-		document.getElementById("i8").src="/images/eight.jpg";
+		document.getElementById("i8").src="images/eight.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image4.src.match("on") && image1.src.match("off") && image3.src.match("off"))
 	{
 		console.log(1);
-		document.getElementById("i8").src="/images/one.jpg";
+		document.getElementById("i8").src="images/one.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image2.src.match("on") && image1.src.match("off"))
 	{
 		console.log(4);
-		document.getElementById("i8").src="/images/four.jpg";
+		document.getElementById("i8").src="images/four.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image3.src.match("off") && image1.src.match("off"))
 	{
 		console.log(0);
-		document.getElementById("i8").src="/images/zero.jpg";
+		document.getElementById("i8").src="images/zero.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image3.src.match("on") && image1.src.match("on") && image4.src.match("off") && image2.src.match("off"))
 	{
 		console.log(10);
-		document.getElementById("i8").src="/images/ten.jpg";
+		document.getElementById("i8").src="images/ten.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image3.src.match("on") && image1.src.match("on") && image4.src.match("on") && image2.src.match("off"))
 	{
 		console.log(7);
-		document.getElementById("i8").src="/images/eleven.jpg";
+		document.getElementById("i8").src="images/eleven.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image1.src.match("on") && image2.src.match("on") && image4.src.match("off") && image3.src.match("off"))
 	{
 		console.log(12);
-		document.getElementById("i8").src="/images/twelve.jpg";
+		document.getElementById("i8").src="images/twelve.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image1.src.match("on") && image2.src.match("on") && image4.src.match("on") && image3.src.match("off"))
 	{
 		console.log(13);
-		document.getElementById("i8").src="/images/thirt.jpg";
+		document.getElementById("i8").src="images/thirt.jpg";
 	}
 	else if (image5.src.match("on") && image6.src.match("on") && image1.src.match("on") && image2.src.match("on") && image3.src.match("on") && image4.src.match("off"))
 	{
 		console.log(14);
-		document.getElementById("i8").src="/images/fort.jpg";
+		document.getElementById("i8").src="images/fort.jpg";
 	}else if (image5.src.match("on") && image6.src.match("on") && image1.src.match("on") && image2.src.match("on") && image3.src.match("on") && image4.src.match("on"))
 	{
 		console.log(3);
-		document.getElementById("i8").src="/images/img3.jpg";
+		document.getElementById("i8").src="images/img3.jpg";
 	}
 	else if (image5.src.match("off") && image6.src.match("on") && image1.src.match("off") && image2.src.match("off") && image3.src.match("off") && image4.src.match("off"))
 	{
 		console.log(8);
-		document.getElementById("i8").src="/images/eight.jpg";
+		document.getElementById("i8").src="images/eight.jpg";
 	}
 	else
 	{
 	console.log(3);
-		document.getElementById("i8").src="/images/img3.jpg";	
+		document.getElementById("i8").src="images/img3.jpg";	
 	}
 }
 function a1() {
     	var image = document.getElementById('i1');		
     	if (image.src.match("off")) 
 	{
-        image.src = "/images/on.png";		
+        image.src = "images/on.png";		
     	} 
 	else
 	{
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     	}
 s3();
 }
@@ -114,10 +114,10 @@ s3();
 function a2() {
     var image = document.getElementById('i2');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
 	
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
@@ -125,45 +125,45 @@ s3();
 function a3() {
     var image = document.getElementById('i3');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
 function a4() {
     var image = document.getElementById('i4');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
 function a5() {
     var image = document.getElementById('i5');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
 function a6() {
     var image = document.getElementById('i6');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
 function a7() {
     var image = document.getElementById('i7');
     if (image.src.match("off")) {
-        image.src = "/images/on.png";
+        image.src = "images/on.png";
     } else {
-        image.src = "/images/off.png";
+        image.src = "images/off.png";
     }
 s3();
 }
